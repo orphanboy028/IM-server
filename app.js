312130276fd5e02d4a349676ea2e8b2ff54201cd
sanjay <orphanboy028@gmail.com>
2023-02-19 18:05:09 +0530

    add utils folder add global error Handeling and catchAsync function for async

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./utils/errorController");
 const userRoutes = require("./routes/userRoutes");
 
 // middleware
@@ -19,5 +21,12 @@ app.use((req, res, next) => {
 
 // API ROUTES
 app.use("/api/v1/indiamart/user", userRoutes);
+
+// Route is not defined Error midelwear
+app.all("*", (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,10 @@
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
 // GEt ALL User Handel By Super Admin- or other permisions users
-exports.getAllUser = async (req, res, next) => {
+exports.getAllUser = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: "Success",
     requesteAt: req.requestTime,
   });
-};
+});
